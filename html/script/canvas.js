d62f9d40e618ecc7b952f4a2736d2d5c3fa9433c
Micho <github@michael-kreil.de>
2012-12-28 05:06:45 +0100

    html magic
    magic = zu viel geändert, um das alles kurz zu beschreiben

--- html/script/canvas.js
+++ html/script/canvas.js
@@ -1,6 +1,7 @@
 function Canvas(options) {
 	var me = this;
 	
+	var imageNode = options.imageNode;
 	var node = options.node;
 	var nodeOverlay = options.nodeOverlay;
 	
@@ -9,18 +10,74 @@ function Canvas(options) {
 	
 	var data = options.data;
 	var columns = options.columns;
+	var rows = (data.length/columns);
 	var thumbWidth = options.thumbWidth;
 	var thumbHeight = options.thumbHeight;
 	
-	me.filter = function (options) {
+	var width = thumbWidth*columns;
+	var height = thumbHeight*rows;
+	
+	var projectX  = [];
+	var projectY  = [];
+	var projectX2 = [];
+	var projectY2 = [];
+	
+	var indexes = [];
+	for (var i = 0; i < data.length; i++) {
+		data[i].id = i;
+		indexes[i] = { entry:data[i], sortBy:i, id:i };
+		projectX[i] = (i % columns)*thumbWidth;
+		projectY[i] = Math.floor(i/columns)*thumbHeight;
+		projectX2[i] = Math.floor(i/rows)*thumbWidth;
+		projectY2[i] = (i % rows)*thumbHeight;
+	}
+
+	me.reset = function () {
+		context.clearRect(0, 0, width, height);
+	}
+	
+	me.sort = function (options) {
 		for (var i = 0; i < data.length; i++) {
-			var entry = data[i];
-			var show = options.callback(entry);
-			context.fillStyle = show ? options.colorTrue : options.colorFalse;
+			indexes[i].sortBy = options.callback(indexes[i].entry);
+		}
+		
+		indexes.sort(function (a, b) { return (a.sortBy == b.sortBy) ? (b.viewCount - a.viewCount) : ((a.sortBy < b.sortBy) ? -1 :  1); });
+		
+		var image = imageNode[0];
+		for (var i = 0; i < indexes.length; i++) {
+			var entry = indexes[i].entry;
+			var id = entry.id;
 			var x = i % columns;
 			var y = Math.floor(i/columns);
-			context.fillRect(x*thumbWidth, y*thumbHeight, thumbWidth, thumbHeight);
+			
+			context.drawImage(
+				image,
+				projectX2[id],
+				projectY2[id],
+				thumbWidth,
+				thumbHeight,
+				projectX2[i],
+				projectY2[i],
+				thumbWidth,
+				thumbHeight
+			);
 		}
+	}
+	
+	me.flag = function (options) {
+		var colorTrue  = generateRGBA(options.colors[1]);
+		var colorFalse = generateRGBA(options.colors[0]);
+		for (var i = 0; i < indexes.length; i++) {
+			var entry = indexes[i].entry;
+			var show = options.callback(entry);
+			
+			context.fillStyle = show ? colorTrue : colorFalse;
+			context.fillRect(projectX2[i], projectY2[i], thumbWidth, thumbHeight);
+		}
+	}
+	
+	function generateRGBA(array) {
+		return 'rgba('+array.join(',')+')';
 	}
 	
 	return me;
--- html/script/main.js
+++ html/script/main.js
@@ -1,28 +1,83 @@
 var canvas;
 
-var colorRed   = 'rgba(237,  28,  36, 0.5)';
-var colorBlue  = 'rgba(171, 199, 255, 0.8)';
-var colorGreen = 'rgba(204, 255, 178, 0.8)';
+var colorRed   = [237,  28,  36, 0.6];
+var colorBlue  = [171, 199, 255, 0.8];
+var colorGreen = [204, 255, 178, 0.8];
+var colorWhite = [255, 255, 255, 0.8];
 
 $(function () {
+	for (var i = 0; i < data.length; i++) {
+		var entry = data[i];
+		entry.publishedTS = (new Date(entry.published)).getTime();
+		entry.updatedTS   = (new Date(entry.updated)).getTime();
+	}
+	
 	canvas = new Canvas({
-		node:$('#gridCanvas'),
-		nodeOverlay:$('#gridCanvasOverlay'),
-		thumbWidth:24,
-		thumbHeight:18,
-		columns:40,
-		data:data
-	});
-	canvas.filter({
-		callback: function (entry) { return entry.restriction },
-		colorTrue:  colorRed,
-		colorFalse: colorGreen,
+		imageNode: $('#gridImage'),
+		node: $('#gridCanvas'),
+		nodeOverlay: $('#gridCanvasOverlay'),
+		thumbWidth: 24,
+		thumbHeight: 18,
+		columns: 40,
+		data: data
 	});
 	
-	$('#gridContainer a').click(function (e) {
-  		e.preventDefault();
-  		$(this).tab('show');
+	$('#gridFlag button').click(function (e) {
+		updateCanvas({ flagType:$(e.target).attr('value') });
 	})
 	
+	$('#gridSort button').click(function (e) {
+		updateCanvas({ sortType:$(e.target).attr('value') });
+	})
+	
+	updateCanvas();
 	//$('#gridContainer a[href="#blocked_in_germany"]').click();
 });
+
+function updateCanvas(options) {
+	canvas.reset();
+	options = options || {};
+	var sortType = options.sortType || $('#gridSort .active').attr('value');
+	
+	var callback;
+	var sortDesc = false;
+	switch (sortType) {
+		case 'views':
+			callback = function (entry) { return -entry.viewCount };
+		break;
+		case 'title':
+			callback = function (entry) { return entry.title.toLowerCase() };
+		break;
+		case 'category':
+			callback = function (entry) { return entry.category.toLowerCase() };
+		break;
+		case 'date':
+			callback = function (entry) { return entry.published };
+		break;
+		case 'rating':
+			callback = function (entry) { return -entry.rating };
+		break;
+	};
+	
+	canvas.sort({
+		callback: callback,
+	});
+	
+	
+	var flagType = options.flagType || $('#gridFlag .active').attr('value');
+
+	switch (flagType) {
+		case 'germany':
+			callback = function (entry) { return entry.restriction };
+		break;
+		case 'precautionary':
+			callback = function (entry) { return (entry.reason.indexOf('möglicherweise') >= 0) };
+		break;
+	};
+	if (flagType != 'none') {
+		canvas.flag({
+			callback: callback,
+			colors: [colorWhite, colorRed]
+		});
+	}
+}
