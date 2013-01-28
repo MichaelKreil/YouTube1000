973f55462cbf442ee2de9b5aa443bdcfb888ebe8
Micho <github@michael-kreil.de>
2013-01-28 02:39:11 +0100

    replace "powered by"

--- html/assets/js/helper.js
+++ html/assets/js/helper.js
@@ -36,11 +36,11 @@ $(document).ready(function(){
 
 			if ($lang == "de") {
 				var text   = 'Die geblockten Top-1000-YouTube-Videos';
-				var suffix = 'Powered by <a href="http://www.myvideo.de">MyVideo</a>. Realisiert von <a href="http://www.opendatacity.de/">OpenDataCity</a>. Anwendung steht unter <a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/">CC-BY 3.0</a>.';
+				var suffix = 'Unterstützt durch <a href="http://www.myvideo.de">MyVideo</a>. Realisiert von <a href="http://www.opendatacity.de/">OpenDataCity</a>. Anwendung steht unter <a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/">CC-BY 3.0</a>.';
 				var file   = 'frame.de.html';
 			} else {
 				var text   = 'Top 1000 YouTube Videos';
-				var suffix = 'Powered by <a href="http://www.myvideo.de">MyVideo</a>. Made by <a href="http://www.opendatacity.de/">OpenDataCity</a>. This App is under <a rel="license" href="http://creativecommons.org/licenses/by/3.0/us/">CC-BY 3.0</a>.';
+				var suffix = 'Supported by <a href="http://www.myvideo.de">MyVideo</a>. Made by <a href="http://www.opendatacity.de/">OpenDataCity</a>. This App is under <a rel="license" href="http://creativecommons.org/licenses/by/3.0/us/">CC-BY 3.0</a>.';
 				var file   = 'frame.en.html';
 			}
 
