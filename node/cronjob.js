4308068be1b4401fcc9acd5c2051fe18254aa28e
Micho <github@michael-kreil.de>
2013-02-15 23:11:05 +0100

    improving cronjob.js

--- node/cronjob.js
+++ node/cronjob.js
@@ -2,10 +2,10 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 
 var workers = [
-	{hour: 4, cmd:'node "1 lists.js"'  },
+//	{hour: 4, cmd:'node "1 lists.js"'  },
 	{hour:10, cmd:'node "1 lists.js"'  },
 	{hour:11, cmd:'node "2 analyse.js"'},
-	{hour:16, cmd:'node "1 lists.js"'  },
+//	{hour:16, cmd:'node "1 lists.js"'  },
 	{hour:22, cmd:'node "1 lists.js"'  },
 	{hour:23, cmd:'node "2 analyse.js"'}
 ];
@@ -26,7 +26,7 @@ function check () {
 				exec(workers[i].cmd,
 					function (error, stdout, stderr) {
 						logInfo(stdout);
-						if (stderr && (stderr != '')) logError(stderr);
+						if (stderr && (stderr != '')) logInfo(stderr);
 						if (error  && (error  != '')) logError(error);
 					}
 				);
@@ -40,16 +40,25 @@ setInterval(check, 30*60*1000);
 
 function logDebug(msg) {
 	console.log('\033[32m' + msg + '\033[0m');
-	fs.appendFileSync('./log/debug.log', msg+'\r', 'utf8');
+	fs.appendFileSync('./log/debug.log', array2log(msg), 'utf8');
 }
 
 function logInfo(msg) {
 	console.log('\033[33m' + msg + '\033[0m');
-	fs.appendFileSync('./log/info.log', msg+'\r', 'utf8');
+	fs.appendFileSync('./log/info.log', array2log(msg), 'utf8');
 }
 
 function logError(msg) {
 	console.log('\033[31m' + msg + '\033[0m');
-	fs.appendFileSync('./log/error.log', msg+'\r', 'utf8');
+	fs.appendFileSync('./log/error.log', array2log(msg), 'utf8');
 }
 
+function array2log(lines) {
+	lines = lines.split('\r');
+	var time = (new Date()).toString();
+	var result = '';
+	for (var i = 0; i < lines.length; i++) {
+		result += time + '\t' + lines[i] + '\r';
+	}
+	return result;
+}
